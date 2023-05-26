19eacfe0d17de48657aaf98bd7241c1d91025049
daqiangli <dqli161@163.com>
2023-05-26 11:33:46 +0800

    delete comment

--- cmd/review-middleware/run.go
+++ cmd/review-middleware/run.go
@@ -6,8 +6,8 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/action"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
-	// "github.com/NpoolPlatform/review-manager/pkg/db"
-	// "github.com/NpoolPlatform/review-middleware/api"
+	"github.com/NpoolPlatform/review-middleware/api"
+	"github.com/NpoolPlatform/review-middleware/pkg/db"
 
 	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
 
