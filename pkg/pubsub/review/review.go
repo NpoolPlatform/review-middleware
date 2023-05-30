b6a84d931e151cc89e90097587a72b3994c1842a
daqiangli <dqli161@163.com>
2023-05-30 16:15:00 +0800

    change referance & update pubsub

--- pkg/pubsub/review/review.go
+++ pkg/pubsub/review/review.go
@@ -5,12 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 
-	reviewmgrpb "github.com/NpoolPlatform/message/npool/review/mw/v2"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/review/mw/v2"
 	review1 "github.com/NpoolPlatform/review-middleware/pkg/mw/review"
 )
 
 func Prepare(body string) (interface{}, error) {
-	req := reviewmgrpb.ReviewReq{}
+	req := npool.ReviewReq{}
 	if err := json.Unmarshal([]byte(body), &req); err != nil {
 		return nil, err
 	}
@@ -18,12 +19,29 @@ func Prepare(body string) (interface{}, error) {
 }
 
 func Apply(ctx context.Context, req interface{}) error {
-	_req, ok := req.(*reviewmgrpb.ReviewReq)
+	_req, ok := req.(*npool.ReviewReq)
 	if !ok {
 		return fmt.Errorf("invalid request")
 	}
 
-	if _, err := review1.CreateReview(ctx, _req); err != nil {
+	handler, err := review1.NewHandler(ctx,
+		review1.WithAppID(_req.AppID),
+		review1.WithDomain(_req.Domain),
+		review1.WithReviewerID(_req.ReviewerID),
+		review1.WithObjectID(_req.ObjectID),
+		review1.WithObjectType(_req.ObjectType),
+		review1.WithTrigger(_req.Trigger),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"Apply",
+			"Req", req,
+			"Error", err,
+		)
+		return err
+	}
+
+	if _, err := handler.CreateReview(ctx); err != nil {
 		return err
 	}
 
