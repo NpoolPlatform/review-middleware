33f8ef209cbd99def99b33af5739674f61cf02b8
daqiangli <dqli161@163.com>
2023-05-30 15:48:57 +0800

    add create

--- pkg/mw/review/create.go
+++ pkg/mw/review/create.go
@@ -2,23 +2,16 @@ package review
 
 import (
 	"context"
-	"fmt"
 
 	npool "github.com/NpoolPlatform/message/npool/review/mw/v2"
-
 	"github.com/NpoolPlatform/review-middleware/pkg/db"
 	"github.com/NpoolPlatform/review-middleware/pkg/db/ent"
-	entreview "github.com/NpoolPlatform/review-middleware/pkg/db/ent/review"
 
 	crud "github.com/NpoolPlatform/review-middleware/pkg/crud/review"
 )
 
-type createHandler struct {
-	*Handler
-}
-
-func (h *createHandler) createReview(ctx context.Context) error {
-	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+func (h *Handler) CreateReview(ctx context.Context) (info *npool.Review, err error) {
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
 		info, err := crud.CreateSet(
 			cli.Review.Create(),
 			&crud.Req{
@@ -37,43 +30,8 @@ func (h *createHandler) createReview(ctx context.Context) error {
 		return nil
 	})
 	if err != nil {
-		return err
-	}
-	return nil
-}
-func (h *Handler) CreateReview(ctx context.Context) (*npool.Review, error) {
-	err := db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		exist, err := tx.
-			Review.
-			Query().
-			Where(
-				entreview.AppID(*h.AppID),
-				entreview.Domain(*h.Domain),
-				entreview.ObjectID(*h.ObjectID),
-				entreview.ObjectType(h.ObjectType.String()),
-				entreview.Or(
-					entreview.State(npool.ReviewState_Wait.String()),
-					entreview.State(npool.ReviewState_Approved.String()),
-				),
-			).
-			Exist(_ctx)
-		if err != nil {
-			return err
-		}
-		if exist {
-			return fmt.Errorf("object review exist")
-		}
-
-		handler := &createHandler{
-			Handler: h,
-		}
-		if err := handler.createReview(ctx); err != nil {
-			return err
-		}
-		return err
-	})
-	if err != nil {
 		return nil, err
 	}
+
 	return h.GetReview(ctx)
 }
