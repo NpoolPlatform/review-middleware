468fb601f76a3739fccc7adff94fe927a778d731
daqiangli <dqli161@163.com>
2023-05-30 16:32:45 +0800

    update create review

--- pkg/mw/review/create.go
+++ pkg/mw/review/create.go
@@ -2,6 +2,7 @@ package review
 
 import (
 	"context"
+	"fmt"
 
 	npool "github.com/NpoolPlatform/message/npool/review/mw/v2"
 	"github.com/NpoolPlatform/review-middleware/pkg/db"
@@ -10,7 +11,34 @@ import (
 	crud "github.com/NpoolPlatform/review-middleware/pkg/crud/review"
 )
 
+type createHandler struct {
+	*Handler
+}
+
+func (h *createHandler) validate() error {
+	if h.AppID == nil {
+		return fmt.Errorf("invalid app id")
+	}
+	if h.ObjectID == nil {
+		return fmt.Errorf("invalid object id")
+	}
+	if h.Domain == nil {
+		return fmt.Errorf("invalid domain")
+	}
+	if h.ReviewerID == nil {
+		return fmt.Errorf("invalid reviewer id")
+	}
+	return nil
+}
+
 func (h *Handler) CreateReview(ctx context.Context) (info *npool.Review, err error) {
+	handler := &createHandler{
+		Handler: h,
+	}
+	if err := handler.validate(); err != nil {
+		return nil, err
+	}
+
 	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
 		info, err := crud.CreateSet(
 			cli.Review.Create(),
