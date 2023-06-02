99b63ed68f502452666d3441f32410ea08b701dc
daqiangli <dqli161@163.com>
2023-06-02 18:40:34 +0800

    change structure

---
+++ api/review/api.go
@@ -0,0 +1,14 @@
+package review
+
+import (
+	review1 "github.com/NpoolPlatform/message/npool/review/mw/v2"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	review1.UnimplementedMiddlewareServer
+}
+
+func Register(server grpc.ServiceRegistrar) {
+	review1.RegisterMiddlewareServer(server, &Server{})
+}
--- api/review/update.go
+++ api/review/update.go
@@ -1,4 +1,4 @@
-package api
+package review
 
 import (
 	"context"
