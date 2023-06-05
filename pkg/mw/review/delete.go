a446793a4a40a9f406be683b4fd41214e754f901
daqiangli <dqli161@163.com>
2023-06-05 18:33:20 +0800

    add delete review

---
+++ pkg/mw/review/delete.go
@@ -0,0 +1,36 @@
+package review
+
+import (
+	"context"
+	"time"
+
+	npool "github.com/NpoolPlatform/message/npool/review/mw/v2/review"
+	crud "github.com/NpoolPlatform/review-middleware/pkg/crud/review"
+	"github.com/NpoolPlatform/review-middleware/pkg/db"
+	"github.com/NpoolPlatform/review-middleware/pkg/db/ent"
+)
+
+func (h *Handler) DeleteReview(ctx context.Context) (info *npool.Review, err error) {
+	info, err = h.GetReview(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		now := uint32(time.Now().Unix())
+		if _, err := crud.UpdateSet(
+			cli.Review.UpdateOneID(*h.ID),
+			&crud.Req{
+				DeletedAt: &now,
+			},
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
