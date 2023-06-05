8d7c4f8228fda725938a5817e6bf0476fd775bd4
daqiangli <dqli161@163.com>
2023-06-05 16:46:11 +0800

    register api

--- api/review/api.go
+++ api/review/api.go
@@ -1,14 +1,14 @@
 package review
 
 import (
-	review1 "github.com/NpoolPlatform/message/npool/review/mw/v2"
+	"github.com/NpoolPlatform/message/npool/review/mw/v2/review"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
-	review1.UnimplementedMiddlewareServer
+	review.UnimplementedMiddlewareServer
 }
 
 func Register(server grpc.ServiceRegistrar) {
-	review1.RegisterMiddlewareServer(server, &Server{})
+	review.RegisterMiddlewareServer(server, &Server{})
 }
